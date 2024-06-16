e7a5ff8c8e697399c517b33bf1416b77441e8930
Marek Zelinka <mzelinka17@gmail.com>
2024-06-16 17:29:04 +0200

    style active contact

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,9 @@
 import { json, redirect } from "@remix-run/node";
 import {
   Form,
-  Link,
   Links,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -58,7 +58,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`/contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -67,7 +72,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                         <i>No Name</i>
                       )}{" "}
                       {contact.favorite ? <span>★</span> : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
