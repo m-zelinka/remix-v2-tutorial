9738b7702922bcaaf2ebecee51b1232a619a7c46
Marek Zelinka <mzelinka17@gmail.com>
2024-06-16 17:15:41 +0200

    use client-side loading

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import {
   Form,
+  Link,
   Links,
   Meta,
   Outlet,
@@ -38,10 +39,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
           </nav>
